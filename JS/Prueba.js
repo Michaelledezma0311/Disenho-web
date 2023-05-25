985c6e43aac7e60208004057e41a5a325e9673ba
Michaelledezma0311 <michaelledezma0311@gmail.com>
2023-05-24 21:25:00 -0400

    Cambios

--- JS/Prueba.js
+++ JS/Prueba.js
@@ -1,4 +1,4 @@
-function sumar(a, b){
+/*function sumar(a, b){
     var s = a + b;
     document.write("La suma es:" +s+ "<br>");
 }
@@ -7,4 +7,16 @@ function sumar(a, b){
 sumar(10,5);
 sumar(8,7);
 sumar(10.8,5.3);
-sumar("Hola","Mundo");
+sumar("Hola","Mundo");*/
+
+function factorial(n){
+    let c=1;
+    let f=1;
+    while(c<=n){
+        f=f*c;
+        c++;
+    }
+    return f;
+}
+
+document.write(factorial(5));
