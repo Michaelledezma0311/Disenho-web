8bf5b85f71c4a708a6415ed5da7ca7936b9e4cd1
Michaelledezma0311 <michaelledezma0311@gmail.com>
2023-05-24 20:19:17 -0400

    JavaS

---
+++ JS/Prueba.js
@@ -0,0 +1,10 @@
+function sumar(a, b){
+    var s = a + b;
+    document.write("La suma es:" +s+ "<br>");
+}
+//llamar a la funcion
+
+sumar(10,5);
+sumar(8,7);
+sumar(10.8,5.3);
+sumar("Hola","Mundo");
