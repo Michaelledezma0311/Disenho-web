ca051f64adc956e170763244a817769252750e05
Michaelledezma0311 <michaelledezma0311@gmail.com>
2023-05-24 20:21:45 -0400

    Cambios

--- JS/Prueba.js
+++ JS/Prueba.js
@@ -2,8 +2,8 @@ function sumar(a, b){
     var s = a + b;
     document.write("La suma es:" +s+ "<br>");
 }
-//llamar a la funcion
 
+//llamar a la funcion
 sumar(10,5);
 sumar(8,7);
 sumar(10.8,5.3);
