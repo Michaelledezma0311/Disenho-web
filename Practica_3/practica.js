d6f11e7dfa0fde25879139d3dd36bc25fc38bd78
Michaelledezma0311 <michaelledezma0311@gmail.com>
2023-05-28 19:38:20 -0400

    Cambios Ultimos

--- Practica_3/practica.js
+++ Practica_3/practica.js
@@ -1,6 +1,6 @@
 function calcularclima(){
 
-    var clima = document.getElementById("clima").value;
+    let clima = document.getElementById("clima").value;
 
     if(clima >= -10 && clima <= 15){
         alert("Frio");
@@ -12,3 +12,41 @@ function calcularclima(){
         alert("Valor fuera de rango, no se puede evaluar")
     }
 }
+
+function calcularfact(){
+
+    let num = document.getElementById("num").value;
+    let resultado = num;
+    for (let i = 1; i < num; i++) {
+        resultado = resultado * i;
+    }
+    alert(resultado);
+}
+
+function fizzbuzz(){
+    for (let i = 1; i < 101; i++) {
+        if (i % 15 == 0){
+            document.write("FizzBuzz<br/>");
+        }else if (i % 3 == 0){
+            document.write(i+" Fizz<br/>");
+        }else if (i % 5 == 0){
+            document.write(i+" Buzz<br/>");
+        }else{
+            document.write(  i+"<br/>");
+        }
+        
+    }
+}
+
+function Fecha(){
+
+    let d = new Date();
+    let dias = ["Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sabado"];
+    let meses = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
+
+    document.getElementById("Fecha").innerHTML = "Hoy es: "+ dias[d.getDay()]+", " + d.getDate() + " de " + meses[d.getMonth()] + " de " + d.getFullYear();
+
+}
+
+
+
